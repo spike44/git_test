af5e6c0a3f7cb3b661fbd1b9714eac2a203503f7
jongmun0816 <jongmun0816@gmail.com>
2020-09-20 21:13:32 +0900

    초기

---
+++ ShinjinTex_Pda_Api/src/main/java/kr/co/shinjintex/api/ShinjinTexPdaApiApplication.java
@@ -0,0 +1,13 @@
+package kr.co.shinjintex.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShinjinTexPdaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShinjinTexPdaApiApplication.class, args);
+	}
+
+}
---
+++ ShinjinTex_Pda_Api/src/test/java/kr/co/shinjintex/api/ShinjinTexPdaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.shinjintex.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShinjinTexPdaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
